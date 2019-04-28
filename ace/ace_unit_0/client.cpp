aae30b29b9ba5fb7bfea653d2e4b1f843e0ebbf7
wuyuchun <xidiantuoersuo@163.com>
2019-04-28 23:02:58 +0800

    add server client app

--- ace/ace_unit_0/client.cpp
+++ ace/ace_unit_0/client.cpp
@@ -1,4 +1,30 @@
 //
-// Created by kimi on 19-4-28.
+// Created by messi on 19-4-28.
 //
 
+#include <ace/Log_Msg.h>
+#include "ace/Addr.h"
+#include "ace/Init_ACE.h"
+#include "ace/INET_Addr.h"
+#include "ace/SOCK_Connector.h"
+
+#include "ace/SOCK_Acceptor.h"
+
+int main(int argc ,char *argv[]){
+    int ret(0);
+    const char *ip = "192.168.3.4";
+    short port = 60000;
+    ACE_INET_Addr srvr(port,ip);
+    ACE_SOCK_Connector connector;
+    ACE_SOCK_Stream peer;
+
+    //ACE_Time_Value time(0,500);
+    //ret = connector.connect(peer,srvr,&time);
+    ret = connector.connect(peer,srvr);
+    if(ret == -1){
+        ACE_ERROR_RETURN((LM_ERROR,ACE_TEXT("%p\n"),ACE_TEXT("connect")),-1);
+    }
+    peer.send_n("hello world!",12);
+    peer.close();
+    return 0;
+}
--- ace/ace_unit_0/server.cpp
+++ ace/ace_unit_0/server.cpp
@@ -1,4 +1,44 @@
 //
-// Created by kimi on 19-4-28.
+// Created by messi on 19-4-28.
 //
+#include <ace/Log_Msg.h>
+#include "ace/Addr.h"
+#include "ace/Init_ACE.h"
+#include "ace/INET_Addr.h"
 
+#include "ace/SOCK_Acceptor.h"
+
+
+int main(int argc ,char *argv[]){
+
+    ACE_INET_Addr port_to_listen(60000);
+    ACE_SOCK_Acceptor acceptor;
+
+    if(acceptor.open(port_to_listen,1) == -1){
+        ACE_ERROR_RETURN((LM_ERROR,ACE_TEXT("%p\n"),ACE_TEXT("acceptor.open")),-1);
+    }
+
+    while(1){
+        ACE_SOCK_Stream peer;
+        ACE_INET_Addr peer_addr;
+
+        if(acceptor.accept(peer,&peer_addr) == -1){
+            ACE_DEBUG((LM_DEBUG,ACE_TEXT("accept errer\n")));
+        }
+        else{
+            ACE_TCHAR peer_name[MAXHOSTNAMELEN];
+            peer_addr.addr_to_string(peer_name,MAXHOSTNAMELEN);
+            ACE_DEBUG((LM_DEBUG,ACE_TEXT("Connetion from %s\n"),peer_name));
+            char buffer[4096];
+            size_t bytes_received;
+
+            while((bytes_received = peer.recv(buffer, sizeof(buffer))) > 0){
+                buffer[bytes_received]=0;
+                ACE_DEBUG((LM_DEBUG,ACE_TEXT("receiver data:%s\n"),buffer));
+            }
+            peer.close();
+            ACE_DEBUG((LM_DEBUG,ACE_TEXT("close the socket.\n")));
+        }
+    }
+    ACE_NOTREACHED(return  0);
+}
